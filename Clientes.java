e1392c37deebaba07417ad52bd07c0a726a2b5f5
Christyan-Almeida <127900444+Christyan-Almeida@users.noreply.github.com>
2025-10-07 09:18:51 -0300

    Add files via upload

---
+++ Clientes.java
@@ -0,0 +1,60 @@
+package Model;
+
+public class Clientes {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+    private String telefone;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {                     // Getter do email
+        return email;
+    }
+
+    public void setEmail(String email) {           // Setter do email
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
